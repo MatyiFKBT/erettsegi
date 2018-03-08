96a732f5881fd9ef3e7cd73d1d79d516977bd58f
Matyi <matolcsy.matyi@gmail.com>
2018-03-08 11:40:23 +0100

    megoldas2

--- downloader.py
+++ downloader.py
@@ -5,8 +5,7 @@ import shutil
 
 while True:
     os.system('cls') #clear screen
-    be = input("Évjárat, évszak? ") # 2 számjegyet vár a program
-    # mo = input("maj / okt? ") # május/október eldöntése, fontos hogy 3 karakter legyen az input
+    be = input("Évjárat, évszak? szóközzel")
     év = be.split()[0]
     mo = be.split()[1]
     folder = év + mo
@@ -20,22 +19,15 @@ while True:
     url = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + évszak + '_emelt/e_inf_' + év + mo + '_fl.pdf'
     forrasok = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + évszak + '_emelt/e_inffor_' + év + mo + '_fl.zip'
     mpdf = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + évszak + '_emelt/e_inf_' + év + mo + '_ut.pdf'
-    #
-    # else:
-    #     url = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + 'osz_emelt/e_inf_' + év + mo + '_fl.pdf'
-    #     forrasok = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + 'osz_emelt/e_inffor_' + év + mo + '_fl.zip'
-    #     mpdf = 'http://dload.oktatas.educatio.hu/erettsegi/feladatok_20' + év + 'osz_emelt/e_inf_' + év + mo + '_ut.pdf'
-
     print('Mappa kész.\n')
     print(url, 'letöltése folyamatban...')
-    print(forrasok, 'letöltése folyamatban...')
-    print(mpdf, 'letöltése folyamatban...')
-
     r = requests.get(url, allow_redirects=True)
+    print(forrasok, 'letöltése folyamatban...')
     f = requests.get(forrasok, allow_redirects=True)
+    print(mpdf, 'letöltése folyamatban...')
     m = requests.get(mpdf, allow_redirects=True)
 
-    open(forras, 'wb').write(f.content) #forrás letöltése
+    open(forras, 'wb').write(f.content) #forrás írása
     print('Kitömörítés...')
     zipfile.ZipFile(forras, 'r').extractall(év + mo) #új mappa, oda megy az extract
     open(forras, 'wb').close() #becsukjuk a forrásfájlt pythonban, hogy később tudjuk törölni, itt a fájlt lenullázza a python
@@ -48,9 +40,10 @@ while True:
     print('Sikeresen letöltve.')
 # ez a while true -s megoldás elég csúnya, de legalább működik.
     while True:
-        még = input('Szeretnél még mást is letölteni? (i/n): ')
+        még = input('Szeretnél még mást is letölteni? (i/n) | n: ')
         if még in ('i', 'n'):
             break
+        break
         print('Sajnos ezt nem tudtam értelmezni')
     if még == 'i':
         continue
