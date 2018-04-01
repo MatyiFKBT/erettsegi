43de2e69fb0a8beeaed8715abb4f7aae303f978d
peterpalfi <34078206+peterpalfi@users.noreply.github.com>
2018-04-01 18:37:39 +0200

    Add files via upload

--- gui/erettsegi_downloader.py
+++ gui/erettsegi_downloader.py
@@ -1,24 +1,29 @@
 import tkinter as tk
 from tkinter import messagebox
 import pygubu
+import os
+from letolt import letolt
 
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 #globalis_valtozok:
 path = None
-kozep = None
-emelt = None
-tavasz = None
-osz = None
+targy = None
+ev = None
+mo = None
+szint= None
 
-class MyApplication(pygubu.TkApplication):
+class MyApplication:
 
-    def _create_ui(self):
+    def __init__(self):
         self.builder = builder = pygubu.Builder()
-        builder.add_from_file('erettsegi_downloader_gui.ui')
+        builder.add_from_file(os.path.join(CURRENT_DIR, 'erettsegi_mainframe.ui'))
 
-        self.mainwindow = builder.get_object('Frame_1', self.master) 
-        self.filepath = builder.get_object('filepath')
+        self.mainwindow = builder.get_object('mainwindow')
         
+        self.filepath = builder.get_object('filepath')
+        self.evek = builder.get_object('evek')
+        self.targyak = builder.get_object('targyak')
         builder.connect_callbacks(self)
         
     def path_changed(self, event=None):
@@ -26,26 +31,49 @@ class MyApplication(pygubu.TkApplication):
         path = self.filepath.cget('path')
     
     def letoltes_clicked(self):
+        letolt(ev, mo, szint, targy, path)
         messagebox.showinfo('Érettségi Downloader', 'A letöltés elkészült')
 
     def kozep_selected(self):
-        global kozep
-        kozep = 1
+        global szint
+        szint = 'kozep'
 
     def emelt_selected(self):
-        global emelt
-        emelt = 1
+        global szint
+        szint = 'emelt'
 
     def tavasz_selected(self):
-        global tavasz
-        tavasz = 1
-    
+        global mo
+        mo = 'maj'
+        
     def osz_selected(self):
-        global osz
-        osz = 1
+        global mo
+        mo = 'okt'
 
+    def ev_selected(self, event=None):
+        global ev
+        ev = self.evek.get()[2:4]
+
+    def targy_selected(self, event=None):
+        global targy
+        if self.targyak.get() == 'magyar_irodalom':
+            targy = 'magyir'
+        elif self.targyak.get() == 'történelem':
+            targy = 'tort'
+        elif self.targyak.get() == 'matematika':
+            targy = 'mat'
+        elif self.targyak.get() == 'német':
+            targy = 'nemet'
+        elif self.targyak.get() == 'informatika':
+            targy = 'inf'
     
+    def quit(self, event=None):
+        self.mainwindow.quit()
+
+    def run(self):
+        self.mainwindow.mainloop()
+        
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = MyApplication(root)
-    root.mainloop()
+    #root = tk.Tk()
+    app = MyApplication()
+    app.run()
