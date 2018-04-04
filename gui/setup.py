a614572fb74744d939afda3ea9ee44434efe96e3
Matyi <matolcsy.matyi@gmail.com>
2018-04-04 09:27:12 +0200

    setup

---
+++ gui/setup.py
@@ -0,0 +1,23 @@
+﻿from cx_Freeze import setup, Executable
+import os
+os.environ['TCL_LIBRARY'] = "C:\\Users\\matol\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Users\\matol\\AppData\\Local\\Programs\\Python\\Python36-32\\tcl\\tk8.6"
+
+base = None    
+
+executables = [Executable("erettsegi_downloader.py", base=base)]
+
+packages = ["idna"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "Éreccségi",
+    options = options,
+    version = "1",
+    description = 'nincs gond',
+    executables = executables
+)
